4ac1922e3ecec58289882df5c9cd708f3a6a2e18
wladimir-dev <wladimir.gonzalez18@gmail.com>
2023-03-07 15:42:45 -0400

    moved functions from useCart to CartContext

--- src/Components/hooks/useCart.js
+++ src/Components/hooks/useCart.js
@@ -3,58 +3,10 @@ import { CartContext } from '../../context/CartContext'
 
 export const useCart = () => {
 
-    const { cart, setCart } = useContext(CartContext);
+    const { cart, setCart, addCart, updateCart, removeitem, removeAllItems, calculateTotal  } = useContext(CartContext);
 
 
-    const addCart = (count, product) => {
-
-        const indexProduct = cart.findIndex(prod => prod.product.id == product.id);
-
-        if (indexProduct != -1) {
-            alert('ya se encuentra en el carrito')
-            return
-        }
-        else {
-            const productCart = {
-                product: product,
-                count: count
-            }
-            const newProduct = [...cart, productCart];
-            setCart(newProduct);
-        }
-
-
-        console.log('product added')
-
-
-    }
-
-    const updateCart = (count, idProduct) => {
-        console.log(`cantidad del producto ${count}`)
-        if (count == 0) {
-            removeitem(idProduct);
-            return
-        }
-
-        const indexProduct = cart.findIndex(cart => cart.product.id == idProduct);
-        let copyCart = [...cart];
-        copyCart[indexProduct].count = count;
-        setCart(copyCart);
-    }
-
-    const removeitem = (idProduct) => {
-        const auxCart = cart.filter(cart => cart.product.id != idProduct);
-        setCart(auxCart);
-    }
-    const removeAllItems = () => {
-        setCart([]);
-    }
-
-    const calculateTotal = () => {
-        let initialValue = 0;
-
-        return cart?.reduce((acumaltor, index) => acumaltor + (index.count * index.product.price), initialValue);
-    }
+   
 
 
     return { cart, setCart, addCart, updateCart, removeitem, removeAllItems, calculateTotal }
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -11,8 +11,69 @@ export const CartProvider = ({ children }) => {
     // const { state: cart, setState: setCart } = useLocalStorage("cartAudioECommerce", [])
     const [cart, setCart] = useState([]);
 
+    const addCart = (count, product) => {
+
+        const indexProduct = cart.findIndex(prod => prod.product.id == product.id);
+
+        if (indexProduct != -1) {
+            alert('ya se encuentra en el carrito')
+            return
+        }
+        else {
+            const productCart = {
+                product: product,
+                count: count
+            }
+            const newProduct = [...cart, productCart];
+            setCart(newProduct);
+        }
+
+
+        console.log('product added')
+
+
+    }
+
+    const updateCart = (count, idProduct) => {
+        console.log(`cantidad del producto ${count}`)
+        if (count == 0) {
+            removeitem(idProduct);
+            return
+        }
+
+        const indexProduct = cart.findIndex(cart => cart.product.id == idProduct);
+        let copyCart = [...cart];
+        copyCart[indexProduct].count = count;
+        setCart(copyCart);
+    }
+
+    const removeitem = (idProduct) => {
+        const auxCart = cart.filter(cart => cart.product.id != idProduct);
+        setCart(auxCart);
+    }
+    const removeAllItems = () => {
+        setCart([]);
+    }
+
+    const calculateTotal = () => {
+        let initialValue = 0;
+
+        return cart?.reduce((acumaltor, index) => acumaltor + (index.count * index.product.price), initialValue);
+    }
+
     return (
-        <CartContext.Provider value={{ cart, setCart }}>
+        <CartContext.Provider
+            value={
+                {
+                    cart,
+                    setCart,
+                    addCart,
+                    updateCart,
+                    removeitem,
+                    removeAllItems,
+                    calculateTotal
+                }
+            }>
             {children}
         </CartContext.Provider>
     )
